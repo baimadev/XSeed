7e91f24c3d52ea2f5b29069640f92edd34f24d01
xiarupeng <805662209@qq.com>
2022-01-06 15:21:12 +0800

    [feat] 发布maven

---
+++ xseed-plugin/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id ("java-gradle-plugin")
+    id ("maven-publish")
+}
+
+val MAVEN_NAME = "http://nexus.holderzone.cn/nexus/content/repositories/xseed-plugin/"
+
+gradlePlugin {
+    plugins {
+
+        create("standaloneGradlePlugins"){
+            //插件名
+            id = "xseed-plugin"
+            implementationClass = "com.holderzone.library.xseed_plugin.XSeedPlugin"
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava"){
+            groupId = "com.holderzone.library"
+            artifactId = "xseed-plugin"
+            version = "1.0.0"
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            url = uri(MAVEN_NAME)
+            isAllowInsecureProtocol = true
+            credentials {
+                username = "admin"
+                password = "admin123"
+            }
+        }
+    }
+}
+
+tasks {
+
+    processResources {
+        duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    }
+
+}
---
+++ xseed-plugin/src/main/java/com/holderzone/library/xseed_plugin/XSeedPlugin.java
@@ -0,0 +1,12 @@
+package com.holderzone.library.xseed_plugin;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class XSeedPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project target) {
+        System.out.println("start");
+
+    }
+}
